5964d4e0d0fec3b508af945da929869a13aa4f6d
Denis Neves <nevesdenis35@gmail.com>
2025-09-16 22:02:46 -0300

    Cria menu hamburguer e pseudo elementos

---
+++ menu-hamburger.js
@@ -0,0 +1,24 @@
+const btnMenu = document.querySelector("#btnMenu");
+
+// captura o elemento do html
+const linksMenu = document.querySelector(".links-menu");
+
+// btnMenu.addEventListener("click", function () {
+//   linksMenu.classList.toggle("ativo");
+// });
+
+// verificando as classes que existem na lista de classes do elemento
+// console.log(linksMenu.classList);
+
+btnMenu.addEventListener("click", () => {
+  linksMenu.classList.toggle("ativo");
+
+  // usamos o contains para verificar se o elemento contém a classe que queremos analisar
+  if (linksMenu.classList.contains("ativo")) {
+    btnMenu.setAttribute("aria-label", "Fechar menu da navegação");
+    btnMenu.setAttribute("aria-expanded", "true");
+  } else {
+    btnMenu.setAttribute("aria-label", "Abrir menu da navegação");
+    btnMenu.setAttribute("aria-expanded", "false");
+  }
+});
